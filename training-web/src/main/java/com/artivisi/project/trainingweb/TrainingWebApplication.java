4fc41a958a76e931f5a17a993f26d99d83b597a3
Adi Sulistiono <sulistiono.adi@gmail.com>
2017-10-28 11:25:19 +0700

    commit modul web

---
+++ training-web/src/main/java/com/artivisi/project/trainingweb/TrainingWebApplication.java
@@ -0,0 +1,12 @@
+package com.artivisi.project.trainingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingWebApplication.class, args);
+	}
+}
---
+++ training-web/src/main/java/com/artivisi/project/trainingweb/WebConfig.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.artivisi.project.trainingweb;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ *
+ * @author adi
+ */
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+    }
+
+}
